7a3314f868d103a0ef854273b479027d0ec7f489
sdsykes <sdsykes@gmail.com>
2010-05-03 11:06:48 +0300

    Remove unused code portions because slim scrooge does not use the level parameter

--- ext/callsite_hash.c
+++ ext/callsite_hash.c
@@ -19,34 +19,22 @@ static int strhash(register const char *string) {
 static VALUE rb_f_callsite( VALUE obj ) {
   struct FRAME *frame = ruby_frame;
   NODE *n;
-  int lev = -1;
   int csite = 0;
 
   if (frame->last_func == ID_ALLOCATOR) {
     frame = frame->prev;
   }
-  if (lev < 0) {
-    ruby_set_current_source();
-    if (frame->last_func) {
-      csite += strhash(ruby_sourcefile) + ruby_sourceline + frame->last_func; 
-    }
-    else if (ruby_sourceline == 0) {
-      csite += strhash(ruby_sourcefile);
-    }
-    else {
-      csite += strhash(ruby_sourcefile) + ruby_sourceline; 
-    }
-    if (lev < -1) return INT2FIX(csite);
+  ruby_set_current_source();
+  if (frame->last_func) {
+    csite += strhash(ruby_sourcefile) + ruby_sourceline + frame->last_func; 
+  }
+  else if (ruby_sourceline == 0) {
+    csite += strhash(ruby_sourcefile);
   }
   else {
-    while (lev-- > 0) {
-      frame = frame->prev;
-      if (!frame) {
-        csite = 0;
-        break;
-      }
-    }
+    csite += strhash(ruby_sourcefile) + ruby_sourceline; 
   }
+
   for (; frame && (n = frame->node); frame = frame->prev) {
     if (frame->prev && frame->prev->last_func) {
       if (frame->prev->node == n) {
