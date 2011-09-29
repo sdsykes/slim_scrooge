fd8398dc26f45412805a113af136b5aec7d63307
Adam Holt <Ads.noob@gmail.com>
2011-09-29 11:12:03 +0200

    Use a block with chdir, as this will restore the original working directory after the command has been executed.
    Fixes #16

--- rails/init.rb
+++ rails/init.rb
@@ -1,7 +1,8 @@
 unless /mswin/ =~ RUBY_PLATFORM || RUBY_VERSION >= "1.9" || defined?(RUBY_ENGINE) && RUBY_ENGINE != "ruby"
   unless File.exists?(File.join(File.dirname(__FILE__), "..", "ext", "Makefile"))
-    Dir.chdir(File.join(File.dirname(__FILE__), "..", "ext"))
-    `rake`
+    Dir.chdir(File.join(File.dirname(__FILE__), "..", "ext")) do
+      `rake`
+    end
   end
 end
 
